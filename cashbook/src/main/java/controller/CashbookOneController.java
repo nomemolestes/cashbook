98f57a78d0a180fc2cf629a9ecd5f7b7ea7786fc
nomemolestes <100700344+nomemolestes@users.noreply.github.com>
2022-04-24 23:01:19 +0900

    [김하늬] cashbookOne controller 수정

--- cashbook/src/main/java/controller/CashbookOneController.java
+++ cashbook/src/main/java/controller/CashbookOneController.java
@@ -19,11 +19,20 @@ public class CashbookOneController extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int cashbookNo = Integer.parseInt(request.getParameter("cashbookNo"));
+		System.out.println(cashbookNo+ "<-cashbookNo");//디버깅
+
 		CashbookDao cashbookDao = new CashbookDao();
 		Cashbook cashbook = new Cashbook();
 		cashbook = cashbookDao.selectCashbookOne(cashbookNo);
-		request.setAttribute("cashbook", cashbook);
-		request.setAttribute("cashbookNo", cashbookNo);
+		
+		request.setAttribute("cashbookNo", cashbook.getCashbookNo());//선택한 요소의 속성이름, 속성값
+		request.setAttribute("cashDate", cashbook.getCashDate());
+		request.setAttribute("kind", cashbook.getKind());
+		request.setAttribute("cash", cashbook.getCash());
+		request.setAttribute("memo", cashbook.getMemo());
+		request.setAttribute("updateDate", cashbook.getUpdateDate());
+		request.setAttribute("createDate", cashbook.getCreateDate());
+		
 		//뷰 포워딩
 		request.getRequestDispatcher("/webapp/view/cashbookOne.jsp").forward(request, response);
 		}
