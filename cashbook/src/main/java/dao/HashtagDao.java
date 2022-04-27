e42e7a9769d70a1cf42e17363c1818325e01fd94
GDJ46 <GDJ46@DESKTOP-QAIORIN>
2022-04-27 17:09:38 +0900

    [김하늬] 해시태그 구현중 hashtag vo 생성, hashtah dao 생성

--- cashbook/src/main/java/dao/HashtagDao.java
+++ cashbook/src/main/java/dao/HashtagDao.java
@@ -10,6 +10,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import vo.Hashtag;
+
 public class HashtagDao {
 	public List<Map<String, Object>> selectTagRankList() {
 		List<Map<String, Object>> list = new ArrayList<>();
@@ -47,5 +49,42 @@ public class HashtagDao {
 		//반환
 		return list;
 	}
+	//해시태그 목록
+	public List<Map<String, Object>> selectCashbookListByTag(Hashtag hashtag) {
+		//저장할목록설정
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		//초기화
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			//드라이버로딩
+			Class.forName("org.mariadb.jdbc.Driver"); // 드라이브 로딩
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cashbook", "root", "java1234");
+			//텍스트쿼리전송
+			String sql = "select h.tag, c.kind kind, c.cash cash, c.memo memo, c.cash_date cashDate from cashbook c left join hastag h in c.cashbook_no = h.cashbook_no where h.tag=?";
+			//쿼리문저장
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, hashtag.getTag());
+			//결과값 반환
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				//결과값을 맵에 저장
+				Map<String, Object> map = new HashMap<String, Object>();
+				//컬럼값에 따라 데이터타입을 지정해서 저장
+				map.put("tag", rs.getString("tag"));
+				map.put("kind", rs.getString("kind"));
+				map.put("cash", rs.getInt("cash"));
+				map.put("memo", rs.getString("memo"));
+				map.put("cashDate", rs.getString("cashDate"));
+				list.add(map);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+		
+	}
+	//해시태그 많은순..
 
 }
